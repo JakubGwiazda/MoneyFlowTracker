7b228e09fda4c1ea26191d5c82d2297223adb7de
DESKTOP-74JJJ8M\Kuba <gwiazdajakub@wp.pl>
2025-12-03 23:02:54 +0100

    Workflow configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,4 @@ export const environment = {
   supabaseUrl: 'https://bebnkqgtuemvscxdlvsq.supabase.co',
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlYm5rcWd0dWVtdnNjeGRsdnNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzMDkwNDcsImV4cCI6MjA3ODg4NTA0N30.KjYA513AesZsXaXFZrHzGKh7DM0EF-fYGoWdyaZ_42U',
-  // openRouterApiKey powinien być dostarczony przez Supabase Edge Function
-  // openRouterApiKey: '',
 };
